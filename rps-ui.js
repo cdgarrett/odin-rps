ffe18544eb2085e1290cf13dcbe3aec30fa6d8ce
Courtney Garrett <cdgarrett19@gmail.com>
2020-08-27 15:55:09 -0700

    Add animations, user input # of rounds, game reset/disable/start functionality

--- rps-ui.js
+++ rps-ui.js
@@ -7,8 +7,65 @@ const PLAYER_WIN_CLASS = 'far fa-arrow-alt-circle-left';
 const COMP_WIN_CLASS = 'far fa-arrow-alt-circle-right';
 const NO_WINNER_CLASS = 'fas fa-ban';
 
-document.querySelectorAll('button').forEach(btn => btn.addEventListener("click", playOneRound));
+let MAX_WINS = 5;
+let RNDS_INPT = document.querySelector("#rounds")
+let RPS_BTNS = document.querySelectorAll('.rps-btns');
 
+window.onload = function() {
+  RNDS_INPT.value = 5;
+  RNDS_INPT.addEventListener("change", disableButtons);
+
+  RPS_BTNS.forEach(btn => {
+    disableButtons();
+    btn.addEventListener("click", playOneRound)
+  });
+
+  document.querySelector('#start-btn').addEventListener("click", startGame);
+  document.querySelector('#player-mv-icon').addEventListener("animationend", removeAnimation);
+  document.querySelector('#comp-mv-icon').addEventListener("animationend", removeAnimation);
+}
+
+
+/**
+ * Function: disableButtons
+ * Summary:  Sets "disabled" attribute on move selection
+ *           buttons to prevent user from playing.
+ * Used by:  Window refresh/load, round input change listener.
+ * Inputs:   Event e.
+ * Returns:  None.
+*/
+function disableButtons(e)
+{
+  RPS_BTNS.forEach(btn => btn.setAttribute("disabled", "disabled"));
+}
+
+
+/**
+ * Function: startGame
+ * Summary:  Gets user input for number of rounds to play, resets
+ *           all scores and icons via resetGame(), and removes
+ *           disabled attribute from buttons.
+ * Used by:  Button listener for 'Start!' button.
+ * Inputs:   Event e.
+ * Returns:  None.
+*/
+function startGame(e)
+{
+  resetGame();
+  MAX_WINS = document.querySelector("#rounds").value;
+  RPS_BTNS.forEach(btn => btn.removeAttribute("disabled"));
+}
+
+
+/**
+ * Function: playOneRound
+ * Summary:  Gets user move selection based on button clicked, randomly
+ *           selects computer move, and passes moves to checkAndUpdateWinner()
+ *           for comparison.
+ * Used by:  Button listeners for rock/paper/scissors move selections.
+ * Inputs:   Event e.
+ * Returns:  None.
+*/
 function playOneRound(e)
 {
   let selection = e.target;
@@ -28,6 +85,19 @@ function playOneRound(e)
   checkAndUpdateWinner(playerMove, Math.floor(Math.random() * 3));
 }
 
+
+/**
+ * Function: checkAndUpdateWinner
+ * Summary:  Updates the player-mv and computer-mv icons to reflect
+ *           player & computer selections, compares player & computer
+ *           selections to determine a winner, updates winner score,
+ *           and checks for 'game over' condition and calls resetGame
+ *           as necessary.
+ * Used by:  playOneRound()
+ * Inputs:   playerMove, computerMove: numerical values representing
+ *           ROCK/PAPER/SCISSORS constants.
+ * Returns:  None.
+*/
 function checkAndUpdateWinner(playerMove, computerMove)
 {
   let playerIcon = document.querySelector('#player-mv-icon');
@@ -38,25 +108,72 @@ function checkAndUpdateWinner(playerMove, computerMove)
   let computerScore = document.querySelector('#computer-score');
   computerIcon.classList = SYMBOL_CLASSES[computerMove];
 
-  let winnerIcon = document.querySelector('#winner-icon');
   if (playerMove == computerMove)
-    winnerIcon.classList = NO_WINNER_CLASS;
+  {
+    playerIcon.classList += " draw-anim";
+    computerIcon.classList += " draw-anim";
+  }
   else if ((playerMove == ROCK && computerMove == SCISSORS)
         || (playerMove == PAPER && computerMove == ROCK)
         || (playerMove == SCISSORS && computerMove == PAPER))
   {
-    winnerIcon.classList = PLAYER_WIN_CLASS;
+    playerIcon.classList +=  " win-anim";
     playerScore.textContent++;
     
-    if (playerScore.textContent == 5)
+    if (playerScore.textContent == MAX_WINS)
+    {
       alert("You won!");
+      resetGame();
+    }
   }
   else
   {
-    winnerIcon.classList = COMP_WIN_CLASS;
+    computerIcon.classList +=  " win-anim";
     computerScore.textContent++;
 
-    if (computerScore.textContent == 5)
-      alert("Suck it.");
+    if (computerScore.textContent == MAX_WINS)
+    {
+      alert("You lost.");
+      resetGame();
+    }
   }
+}
+
+
+/**
+ * Function: resetGame
+ * Summary:  Resets scores to 0 and icons to starting position.
+ * Used by:  checkAndUpdateWinner()
+ * Inputs:   None.
+ * Returns:  None.
+*/
+function resetGame()
+{
+  let playerScore = document.querySelector('#player-score');
+  let computerScore = document.querySelector('#computer-score');
+
+  let playerIcon = document.querySelector('#player-mv-icon');
+  let computerIcon = document.querySelector('#comp-mv-icon');
+
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+
+  playerIcon.classList = SYMBOL_CLASSES[ROCK] + " start";
+  computerIcon.classList = SYMBOL_CLASSES[ROCK] + " start";
+}
+
+
+
+/**
+ * Function: removeAnimation
+ * Summary:  Removes the animation classes from player &
+ *           computer move icons when the animations end.
+ * Used by:  animation end listeners on move icons.
+ * Inputs:   Event e.
+ * Returns:  None.
+*/
+function removeAnimation(e)
+{
+  e.target.classList.remove("draw-anim");
+  e.target.classList.remove("win-anim");
 }
